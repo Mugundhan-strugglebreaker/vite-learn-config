6f7289c36fa342f2fbc670ad6d5e68d3b8908b67
Mugundhan-strugglebreaker <srmvmugund@gmail.com>
2025-01-26 22:37:03 -0800

    condition based config in vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite"
 
-export default defineConfig( {
-    base: "/vite"
+export default defineConfig( ( {command , mode , isSsrBuild}) => {
+    console.log(command , mode , isSsrBuild)
+    if(mode === "development"){
+        return {
+            base: "/vite"
+        }
+    }
+    if(mode === "production") {
+        return {
+            base: "/prod/vite"
+        }
+    }
+    return {};
 })
