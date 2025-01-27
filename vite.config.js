78f8844df97108c2db19f5972e38c0def571e147
Mugundhan-strugglebreaker <srmvmugund@gmail.com>
2025-01-26 22:46:42 -0800

    Accessing env in config file

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 export default defineConfig( ( {command , mode , isSsrBuild}) => {
     console.log(command , mode , isSsrBuild)
-    if(mode === "development"){
-        return {
-            base: "/vite"
-        }
-    }
-    if(mode === "production") {
-        return {
-            base: "/prod/vite"
-        }
-    }
+    // if(mode === "development"){
+    //     return {
+    //         base: "/vite"
+    //     }
+    // }
+    // if(mode === "production") {
+    //     return {
+    //         base: "/prod/vite"
+    //     }
+    // }
+    
+    
+    // mode , current working directory and prefix 
+    const env = loadEnv(mode , process.cwd(), "VITE_")
+
+    console.log(env)
     return {};
 })
